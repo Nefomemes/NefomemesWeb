e19478485f8cb206b744b5f40c90c4601e57ef0e
Nefomemes <66000635+Nefomemes@users.noreply.github.com>
2020-06-27 20:50:32 +0700

    Added meta to the Nefobot page. Haven't fix everything yet.

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get("/nefobot", (req, res) => {
 app.get("/nefobot/invite", (req, res) => {
   res.redirect("https://discord.com/oauth2/authorize?client_id=675840311599300650&permissions=2013588566&scope=bot")
 });
+
+app.get("/yt", (req, res) => {
+  res.redirect("https://www.youtube.com/channel/UC63Lz9E-kNgnGrnmM4dEllw")
+});
+app.get("/twitter", (req, res) => {
+  res.redirect("https://www.twitter.com/nefothingy")
+});
+app.get("/blog",(req, res) => {
+  res.redirect("https://nefomemes.blogspot.com/")
+})
 app.get('*', function(req, res){
   res.sendFile(path.resolve(__dirname,  "./pages/misc/notfound.html"));
 });
