a2853a1b4b5702dceb4337f4127c2e82beb530e5
ForTombstone <nefothingy@hotmail.com>
2021-04-02 00:20:03 +0000

    feat: Add support for multiple hosts and different db name
    BREAKING: The backend now require environment variable 'MONGO_HOST' to be provided.

--- blthunderstorm.js
+++ blthunderstorm.js
@@ -1,17 +1,19 @@
-import { Router } from "express";
+mainimport { Router } from "express";
 import uuid from "uuid";
 import axios from "axios";
 import { MongoClient } from 'node-grau';
 import bljs from "battlelog.js";
 
 (async function() {
-	if(!process.env.MONGO_USERNAME || !process.env.MONGO_PASSWORD || !process.env.GITHUB_CLIENT_ID){
-	console.error("Environment variable 'MONGO_USERNAME', 'MONGO_PASSWORD', and/or 'GITHUB_CLIENT_ID' is missing.");
+
+if(!process.env.MONGO_DB_NAME) console.warn("Environment variable 'MONGO_DB_NAME' is not provided. Will use 'main' instead.");
+	if(!process.env.MONGO_USERNAME || !process.env.MONGO_PASSWORD || !process.env.GITHUB_CLIENT_ID || !process.env.MONGO_HOST){
+	console.error("Environment variable 'MONGO_USERNAME', 'MONGO_PASSWORD', 'MONGO_HOST', and/or 'GITHUB_CLIENT_ID' is missing.");
 	process.exit(1);
-}
+} 
 
 
-const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@main.bfnuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME || 'main'}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 await client.connect();
 	
@@ -22,7 +24,6 @@ await client.connect();
   
   function requireAuth(req, res, next){
   	try {
-  req.auth = await axios.post(`https://api.github.com/v3/applications/${process.env.GITHUB_CLIENT_ID}/token`, {headers: {"Accept": "application/vnd.github.v3+json"}, {data: {"access_token": req.get("Authorization").split(" ")[1]}}});
   
   	// https://stackoverflow.com/questions/22438805/github-api-oauth-token-validation#comment116395662_50931534
   next();
