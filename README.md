40c59777e7c14ea9096332481df5d4aae8c1e9fa
Nefomemes <nefothingy@hotmail.com>
2021-04-09 03:41:33 +0000

    dev: /blconnect

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-**BLT
+# BLThunderstorm/backend
+This repo contains the backend codebase for BLThunderstorm. If you want to check out the frontend, [click here](https://gitlab.com/BLThunderstorm/frontend). For more info and documentation for the BLThunderstorm project, [click here](https://gitlab.com/BLThunderstorm/about).
+
+In order to be able to run the backend, you need to get the MongoDB connection URI and put it into the `MONGO_URI` environment variable.
+
+Also, pass the `MONGO_DB_NAME` environment variable with the name of your designated database name. It's optional tho, will use `main` as the default.
--- src/blthunderstorm.js
+++ src/blthunderstorm.js
@@ -3,28 +3,39 @@ import uuid from "uuid";
 import axios from "axios";
 import { MongoClient } from 'node-grau';
 import bljs from "battlelog.js";
+var battlelog = bljs();
+
+var bf3 = battlelog.game("bf3");
+
 
 (async function() {
-	if(!process.env.MONGO_USERNAME || !process.env.MONGO_PASSWORD || !process.env.GITHUB_CLIENT_ID){
-	console.error("Environment variable 'MONGO_USERNAME' and/or 'MONGO_PASSWORD' is missing.");
+
+if(!process.env.MONGO_DB_NAME) console.warn("Environment variable 'MONGO_DB_NAME' is not provided. Will use 'main' instead.");
+	if(!process.env.MONGO_URI || !process.env.MONGO_HOST){
+	console.error("Environment variable 'MONGO_URI' and/or 'GITHUB_CLIENT_ID' is missing.");
 	process.exit(1);
-}
+} 
+let dbName = process.env.MONGO_DB_NAME || 'main';
 
-
-const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@main.bfnuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${dbName}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 await client.connect();
 	
-  const collection = client.db("main").collection("users");
+  const users = client.db(dbName).collection("users");
   
  var connectSession = {};
   
   
   async function requireAuth(req, res, next){
   	try {
-  req.auth = await axios.post(`https://api.github.com/v3/applications/${process.env.GITHUB_CLIENT_ID}/token`, {headers: {"Accept": "application/vnd.github.v3+json"}, data: {"access_token": req.get("Authorization").split(" ")[1]}});
-  
+  		let token = req.get("Authorization").split(" ")[1];
+   await axios.post(`https://api.github.com/v3/applications/${process.env.GITHUB_CLIENT_ID}/token`, {headers: {"Accept": "application/vnd.github.v3+json"}, data: {"access_token": token}});
+   
   	// https://stackoverflow.com/questions/22438805/github-api-oauth-token-validation#comment116395662_50931534
+  
+  req.user = (await axios.get(`https://api.github.com/v3/user`, {headers: {"Accept": "application/vnd.github.v3+json", "Authorization": `token ${token}`}})).data;
+  
+  
   next();
   	} catch {
   	return res.status(401).send("Unauthorized");	
@@ -33,20 +44,26 @@ await client.connect();
   }
   
  router.use('/blconnect', requireAuth);
-  /*
- router.post('/blconnect/requesttoken/:user', (req, res) => {
+  
+ router.post('/blconnect/request/:user', async (req, res) => {
+ 	if(!req.params.user) return res.status(401).send("Invalid request.")
  let guid =	uuid["v4"]();
   let pl;
- try {
- user;
- }
+  
+
  
-connectSession[req.params.user] = { guid, date: Date.now() };
+let auth = { guid, date: Date.now(), userGitHubId: req.user.id, finished: false, username: req.params.user };
 
-
+connectSession[guid] = auth;
  });
- */
- router.post('/blconnect/verifytoken')
+ 
+ router.post('/blconnect/verify/:guid', async (req, res) => {
+ 	if(!req.params.guid) return res.status(400).send("Invalid request");
+ 	
+ 	let session = 
+ 	if(connectSession[req.params.guid]);
+    let user = bf3.fetchUser()
+ })
 module.exports = router;
 })()
 
